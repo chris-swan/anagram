2c6d2c4092bef90c30c33a143209aeef36d49214
Jason Bethel & Chris Swan <student@epicodus.com>
2015-08-11 14:20:21 -0700

    complete method to test spec3

--- src/Anagram.php
+++ src/Anagram.php
@@ -5,21 +5,19 @@
        {
          $word_list = strtolower($test);
          $user_word = strtolower($word);
-         $word_list_array = explode(" ", $test);
+         $word_list_array = explode(", ", $word_list);
          $user_word_split = str_split($user_word);
          $output = array();
-         foreach ($word_list_array as $check)
-         { $check_split = str_split($check);
-           if (sort($user_word_split) == sort($check_split)) {
+
+         foreach ($word_list_array as $check) {
+          $check_split = str_split($check);
+          sort($user_word_split);
+          sort($check_split);
+           if ($user_word_split == $check_split) {
                array_push($output, $check);
            }
           }
-                    return ($output);
+                    return implode(", ", $output);
          }
-    //        if ($user_word_array == $word_list_array) {
-    //
-    //            return ($test);
-    //       }
-    // }
   }
 ?>
